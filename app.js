80235aa8e2c4a4c954658898445d5e645abc5b69
vikas-reddy-pagilla <pagilla.vikas.reddy@gmail.com>
2022-02-12 23:36:45 +0530

    express basics

--- app.js
+++ app.js
@@ -1,36 +1,25 @@
 const http = require('http');
-const fs  = require('fs')
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-    const url = req.url
-    const method = req.method
+const app = express()
 
-    if( url === '/'){
-    res.setHeader('Content-Type', 'text/html')
-    res.write('<html>')
-    res.write('<head><title>My First Page</title></html>')
-    res.write('<body><form action="/create-user" method="POST"><input type="text" name="Username"><button type="submit">Send</button></form></body>')
-    res.write('</html>')
-    return res.end()
-    }
+// app.use((req, res, next)=>{
+//     console.log('Hii')
+//     next()
+// })
+
+// app.use((req, res, next)=>{
+//     console.log('How are you?')
+// })
+
+app.use('/user',(req, res, next)=>{
+    res.send('How are you?')
     
-    if(url === '/create-user' && method === 'POST'){
-        const body = []
-        req.on('data', (chunk)=>{
-            body.push(chunk)
-        });
-            return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1]
-            console.log(message)
-            fs.writeFile('message.txt',message, (err)=>{
-                res.statusCode = 302
-                res.setHeader('Location', '/')
-                return res.end()
-            });
-                
-        })
-    }
-});
+})
 
-server.listen(5000)
+app.use('/',(req, res, next)=>{
+    res.send('Hii')
+})
+
+
+app.listen(5000)
